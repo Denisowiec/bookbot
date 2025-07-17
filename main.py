7763bbcec9e5ba3e5f125118c078ae8193be43c5
Denisowiec <ddspeed@gmail.com>
2025-07-17 11:34:44 +0200

    Formatted output

--- main.py
+++ main.py
@@ -1,14 +1,23 @@
-from stats import count_words
-from stats import count_letters
+from stats import *
 
 def get_book_text(fpath):
     with open(fpath) as f:
         return f.read()
     
 def main():
-    src_text = get_book_text("books/frankenstein.txt")
+    src_path = "books/frankenstein.txt"
+    src_text = get_book_text(src_path)
     num_words = count_words(src_text)
-    print(f"{num_words} words found in the document")
-    print(count_letters(src_text))
+    num_letters = sort_letters(count_letters(src_text))
+
+    print(f"""============ BOOKBOT ============
+Analyzing book found at {src_path}...
+----------- Word Count ----------
+Found {num_words} total words
+--------- Character Count ------- """)
+    for l in num_letters:
+        print(f"{l["char"]}: {l["num"]}")
+    
+    print("============= END ===============")
 
 main()
--- stats.py
+++ stats.py
@@ -1,15 +1,36 @@
-alphabet = ('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z')
+# alphabet = ('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z')
 
 def count_words(s):
     return len(s.split())
 
+
+
 def count_letters(s):
     letter_count = {}
-    for l in alphabet:
-        letter_count[l] = 0
 
     for l in s:
-        if l.lower() in alphabet:
-            letter_count[l.lower()] += 1
+        if not l.isalpha():
+            continue
+
+        low = l.lower()
+        if low in letter_count:
+            letter_count[low] += 1
+        else:
+            letter_count[low] = 1
 
     return letter_count
+
+def sort_on(items):
+    # Helper function to facilitate the sorting of a list of dictionaries.
+    return items["num"]
+
+def sort_letters(letters):
+    letter_list = []
+    for i in letters:
+        let_dict = {"char": i, "num": letters[i]} 
+        letter_list.append(let_dict)
+    
+    # Sort the list
+    letter_list.sort(reverse=True, key=sort_on)
+    return letter_list
+
