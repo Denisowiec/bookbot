e3c22c4b657cf6f429f168a6361c27352369bc2d
Denisowiec <ddspeed@gmail.com>
2025-07-17 11:45:29 +0200

    Finished tutorial

--- main.py
+++ main.py
@@ -1,23 +1,34 @@
 from stats import *
+import sys
 
 def get_book_text(fpath):
     with open(fpath) as f:
         return f.read()
     
+def make_formatted_output(path, words, letters):
+    output = f"""============ BOOKBOT ============
+Analyzing book found at {path}...
+----------- Word Count ----------
+Found {words} total words
+--------- Character Count ------- """
+    for l in letters:
+        output += f"\n{l["char"]}: {l["num"]}"
+    
+    output += "\n============= END ==============="
+
+    return output
+    
 def main():
-    src_path = "books/frankenstein.txt"
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    
+    src_path = sys.argv[1]
+
     src_text = get_book_text(src_path)
     num_words = count_words(src_text)
     num_letters = sort_letters(count_letters(src_text))
-
-    print(f"""============ BOOKBOT ============
-Analyzing book found at {src_path}...
------------ Word Count ----------
-Found {num_words} total words
---------- Character Count ------- """)
-    for l in num_letters:
-        print(f"{l["char"]}: {l["num"]}")
+    print(make_formatted_output(src_path, num_words, num_letters))
     
-    print("============= END ===============")
 
 main()
