3439315801484197990db2f22602cbdf39c68895
Denisowiec <ddspeed@gmail.com>
2025-07-17 10:53:39 +0200

    Added count_letters

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 from stats import count_words
+from stats import count_letters
 
 def get_book_text(fpath):
     with open(fpath) as f:
         return f.read()
     
 def main():
-    num_words = count_words(get_book_text("books/frankenstein.txt"))
+    src_text = get_book_text("books/frankenstein.txt")
+    num_words = count_words(src_text)
     print(f"{num_words} words found in the document")
+    print(count_letters(src_text))
 
 main()
--- stats.py
+++ stats.py
@@ -1,2 +1,15 @@
+alphabet = ('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z')
+
 def count_words(s):
     return len(s.split())
+
+def count_letters(s):
+    letter_count = {}
+    for l in alphabet:
+        letter_count[l] = 0
+
+    for l in s:
+        if l.lower() in alphabet:
+            letter_count[l.lower()] += 1
+
+    return letter_count
