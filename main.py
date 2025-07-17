91170a048e336c88f25a50dfb4400cb05e272d2f
Denisowiec <ddspeed@gmail.com>
2025-07-17 10:40:34 +0200

    Refactored the code into multiple files.

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
+from stats import count_words
+
 def get_book_text(fpath):
     with open(fpath) as f:
         return f.read()
     
 def main():
-    print(get_book_text("books/frankenstein.txt"))
+    num_words = count_words(get_book_text("books/frankenstein.txt"))
+    print(f"{num_words} words found in the document")
 
 main()
